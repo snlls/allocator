1feea30c271ea6facaef5f7aad25dbac6e0c0f69
s <s@cell.cell>
2023-07-26 23:22:49 +0300

    update

--- allocator.c
+++ allocator.c
@@ -3,113 +3,214 @@
 #include <string.h>
 #include <stdio.h>
 
+typedef struct allocator_dbg_block allocator_dbg_block;
+struct allocator_dbg_block {
+	allocator_dbg_block *prev;
+	allocator_dbg_block *next;
+	void *p;
+	size_t size;
+	char *file;
+	char *func;
+	int line;
+};
 
-// dump list allocator for debug purposes
-struct adata;
-
-typedef struct adata {
-	void *data;
-	struct adata *next;
-} adata;
-
-static adata global_adata = {0};
-allocator *allocator_global = &global_adata;
-
-void allocator_fail()
+void allocator_dbg_fail(allocator *a ALLOCATOR_PROT)
 {
+	perror("allocator failed, aborting");
 	abort();
 }
-void allocator_init(allocator *a)
+
+
+void allocator_dbg_init(allocator *a)
 {
-	assert(a);
-	memset(a, 0, sizeof(*a));
+	a->alloc = allocator_dbg_alloc;
+	a->resize = allocator_dbg_resize;
+	a->free = allocator_dbg_free;
+	a->fail = allocator_dbg_fail;
+	a->reset = allocator_dbg_reset;
+	a->debug = allocator_dbg_str;
+	a->data = 0;
 }
 
-void allocator_deinit(allocator *a)
+void allocator_dbg_reset(allocator *a)
 {
-	assert(a);	
-	adata *tmp = a;
-	if(tmp->data) {
-		free(tmp->data);
-		tmp->data = 0;
+	allocator_dbg_block *b = a->data;
+	uint count = 0;
+	while(b) {
+		count++;
+		b = b->next;
 	}
-	tmp = tmp->next;
+	if(!count)
+		return;
+	b = a->data;
+	//LOG_MEM_ERR("Destroying dbg allocator\t still allocated %u", count);
+	while(b) {
+		//LOG_MEM_DBG("\t%s:%d %s size %lu", b->file, b->line, b->func, b->size);
+		if(b->file)
+			free(b->file);
+		if(b->func)
+			free(b->func);
+		void *next = b->next;
+		free(b);
+		b = next;
+	}
+	a->data = 0;
+}
+
+void *allocator_dbg_alloc(allocator *a, size_t size ALLOCATOR_PROT)
+{
+	const int l = line;
+	const char *fl = file;
+	const char *fn = func;
 	
-	while(tmp) {
-		if(tmp->data) {
-			free(tmp->data);
-			tmp->data = 0;
+	allocator_dbg_block *b = malloc(sizeof(*b) + size);
+	if(!b) {
+		if(a->fail) {
+			a->fail(a ALLOCATOR_ARGS_CALL);
+			return 0;
 		}
-		adata *t = tmp;
-		tmp = tmp->next;
-		free(t);
+		else {
+			perror("allocation failed");
+			abort();
+		}
+	}		
+	b->file = strdup(fl);
+	b->func = strdup(fn);
+	b->line = l;
+	b->size = size;
+	b->p = (b + 1);
+	if(!a->data) {
+		a->data = b;
+		b->next = 0;
+		b->prev = 0;
 	}
+	else {
+		allocator_dbg_block *last = a->data;
+		while(last->next)
+			last = last->next;
+		last->next = b;
+		b->prev = last;
+		b->next = 0;
+	}
+	return (b + 1);
 }
-
-
-/** @brief print debug info to buffer */
-char *allocator_str(allocator *a, char *buf, int buf_len)
+void *allocator_dbg_resize(allocator*a, void *p, size_t size ALLOCATOR_PROT)
 {
-	assert(a);
-	assert(buf);
-	int nodes = 0;
-	int allocs = 0;
-	adata *tmp = a;
-	while(tmp) {
-		nodes++;
-		if(tmp->data)
-			allocs++;
-		tmp = tmp->next;
+	if(!size) {
+		allocator_dbg_free(a, p ALLOCATOR_ARGS_CALL);
+		return 0;
 	}
-	(void)snprintf(buf, buf_len, "allocator: nodes %d allocs %d", nodes, allocs);
+	if(!p) {
+		return allocator_dbg_alloc(a, size ALLOCATOR_ARGS_CALL);
+	}
+	allocator_dbg_block *b = ((allocator_dbg_block*)p) - 1;
+	void *newp = allocator_dbg_alloc(a, size ALLOCATOR_ARGS_CALL);
+	
+	memcpy(newp, b->p, size < b->size ? size : b->size);
+	allocator_dbg_free(a, p ALLOCATOR_ARGS_CALL);
+	return newp;
+}
+void allocator_dbg_free(allocator *a, void *p ALLOCATOR_PROT)
+{
+	if(!p)
+		return;
+	allocator_dbg_block *b = ((allocator_dbg_block*)p) - 1;
+	free(b->file);
+	free(b->func);
+	allocator_dbg_block *prev = b->prev;
+	if(prev)
+		prev->next = b->next;
+	else
+		a->data = b->next;
+	if(b->next)
+		b->next->prev = prev;
+	free(b);
+	
+}
+char *allocator_dbg_str(allocator *a, char *buf, int len)
+{
+	allocator_dbg_block *b = a->data;
+	uint count = 0;
+	while(b) {
+		count++;
+		b = b->next;
+	}
+	char *c = buf;
+	int tmp = snprintf(c, len, "dbg allocator%p count %u\n", a, count);
+	c += tmp;
+	len -= tmp;
+	b = a->data;
+	while(b && len > 0) {
+		tmp = snprintf(c, len, "\n\t%s:%d %s size %lu", b->file, b->line, b->func, b->size);
+		c += tmp;
+		len -= tmp;
+		b = b->next;
+	}
 	return buf;
 }
 
-void *allocator_alloc(allocator *a, size_t size)
+void allocator_heap_fail(allocator *a ALLOCATOR_PROT)
 {
-	assert(a);
-	if(!size)
-		return 0;
-	adata *tmp = a;
-	while(tmp) {
-		if(!tmp->data) {
-			tmp->data = malloc(size);
-			if(!tmp->data)
-				allocator_fail();
-			
-			return tmp->data;
-		}
-		if(!tmp->next) {
-			tmp->next = malloc(sizeof(adata));
-			if(!tmp->next) {
-				allocator_fail();
-				return 0;
-			}
-			tmp->next->next = 0;
-			tmp->next->data = malloc(size);
-			if(!tmp->next->data) {
-				allocator_fail();
-				return 0;
-			}
-			return tmp->next->data;
-		}
-		tmp = tmp->next;
-	}
-	return 0;
+	perror("allocator failed, aborting");
+	abort();
 }
 
-void allocator_free(allocator *a, void *p)
+void allocator_heap_init(allocator *a)
 {
-	assert(a);
-	if(!p)
-		return;
-	adata *tmp = a;
-	while(tmp) {
-		if(tmp->data == p) {
-			free(tmp->data);
-			tmp->data = 0;
-			return;
+	memset(a, 0, sizeof(*a));
+	a->free = allocator_heap_free;
+	a->alloc = allocator_heap_alloc;
+	a->resize = allocator_heap_resize;
+	a->fail = allocator_heap_fail;
+	a->reset = allocator_heap_reset;
+	a->debug = allocator_heap_str;
+}
+
+void allocator_heap_reset(allocator *a)
+{
+	allocator_heap_clear(a);
+}
+
+
+void allocator_heap_clear(allocator *a)
+{
+	(void)a;
+}
+
+void *allocator_heap_alloc(allocator *a, size_t s ALLOCATOR_PROT)
+{
+	(void)a;
+	void *p = calloc(1, s);
+	if(!p) {
+		if(a->fail) {
+			a->fail(a ALLOCATOR_ARGS_CALL);
+			return 0;
 		}
-		tmp = tmp->next;
+		else {
+			perror("allocation failed");
+			abort();
+			return 0;
+		}
 	}
 }
+
+void *allocator_heap_resize(allocator *a, void *p, size_t s ALLOCATOR_PROT) 
+{
+	(void)a;
+	return realloc(p, s);
+}
+
+void allocator_heap_free(allocator *a, void *p ALLOCATOR_PROT)
+{
+	(void)a;
+	free(p);
+}
+
+char *allocator_heap_str(allocator *a, char *buf, int len)
+{
+	if(len)
+		buf[0] = 0;
+	
+	return buf;
+}
+
--- allocator.h
+++ allocator.h
@@ -1,21 +1,54 @@
-
-
+#pragma once
 #include <stdlib.h>
 
-typedef void allocator;
+#ifndef NDEBUG
 
-void allocator_fail() __attribute__((weak));	
+#define ALLOCATOR_PROT , const char *file, int line, const char *func
+#define ALLOCATOR_ARGS ,__FILE__, __LINE__, __func__
+#define ALLOCATOR_ARGS_CALL ,file, line, func
 
-void allocator_init(allocator *a);
-void allocator_denit(allocator *a);
+#else
 
-/** @brief print debug info to buffer */
-char *allocator_str(allocator *a, char *buf, int buf_len);
+#define ALLOCATOR_PROT
+#define ALLOCATOR_ARGS
+#define ALLOCATOR_ARGS_CALL
 
-void *allocator_alloc(allocator *a, size_t size);
-void allocator_free(allocator *a, void *p);
+#endif
 
-extern allocator *allocator_global;
+typedef struct allocator allocator;
+struct allocator {
+	void *(*alloc)(allocator *a, size_t ALLOCATOR_PROT);
+	void *(*resize)(allocator *a, void*, size_t ALLOCATOR_PROT);
+	void (*free)(allocator *a, void* ALLOCATOR_PROT);
+	void (*fail)(allocator *a ALLOCATOR_PROT);
+	void (*reset)(allocator *a);
+	char *(*debug)(allocator *a, char *buf, int buf_len);
+	void *data;
+};
 
-#define allocate(size) allocator_alloc(allocator_global, (size))
-#define deallocate(p) allocator_free(allocator_global, (p))
+#define allocator_alloc(a, size) (a)->alloc(a, (size) ALLOCATOR_ARGS)
+#define allocator_resize(a, p, size) (a)->resize((a), (p), (size) ALLOCATOR_ARGS)
+#define allocator_free(a, p) (a)->free((a), (p) ALLOCATOR_ARGS)
+#define allocator_reset(a) do { if((a)->reset) (a)->reset((a)); } while(0)
+#define allocator_debug_str(a, buf, len) (a)->debug ? (a)->debug((a), (buf), (len)) : 0
+
+
+void allocator_heap_init(allocator *a);
+void allocator_heap_reset(allocator *a);
+void allocator_heap_clear(allocator *a);
+void *allocator_heap_alloc(allocator *a, size_t s ALLOCATOR_PROT);
+void *allocator_heap_resize(allocator *a, void *p, size_t s ALLOCATOR_PROT);
+void allocator_heap_free(allocator *a, void *p ALLOCATOR_PROT);
+char *allocator_heap_str(allocator *a, char *buf, int len);
+void allocator_heap_fail(allocator *a ALLOCATOR_PROT);
+
+void allocator_dbg_init(allocator *a);
+void allocator_dbg_reset(allocator *a);
+void allocator_dbg_clear(allocator *a);
+void *allocator_dbg_alloc(allocator *a, size_t s ALLOCATOR_PROT);
+void *allocator_dbg_resize(allocator *a, void *p, size_t s ALLOCATOR_PROT);
+void allocator_dbg_free(allocator *a, void *p ALLOCATOR_PROT);
+char *allocator_dbg_str(allocator *a, char *buf, int len);
+void allocator_dbg_fail(allocator *a ALLOCATOR_PROT);
+
+
